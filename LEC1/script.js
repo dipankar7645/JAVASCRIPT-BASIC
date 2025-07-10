85d89891519dc51fa82f4b480cad6752fd41d270
dipankar7645 <dipankar11092003@gmail.com>
2025-07-11 01:08:35 +0530

    commit

---
+++ LEC1/script.js
@@ -0,0 +1,48 @@
+// var, let, const - line-by-line comparison
+// decclaration and initialization
+
+
+// var a; // delcare karna
+// var a = 12; // declare and initialize
+
+// window mein add hota hai
+// function scoped hota hai
+// aap fir se declare kar sakte ho same name se and error nahi aayega 
+
+// console.log("Hello")
+// console.log("Dipankar")
+// console.log("Kumar")
+// console.log("JI")
+// console.log("HII")
+
+// a=20;
+// console.log(a);
+
+// re-declare and updation is possible , glabal scope
+
+// var a = 10;
+// var a = 20;
+// console.log(a);
+
+// let a; // declare
+// a = 5; // initialization
+// let b = 12; // dono kaam ek saath karna
+
+// updation is possible but not redeclaration local scope
+
+// let a = 15;
+//  a = 20;
+// console.log(a);
+
+// updation and redeclaration is not possible local scope
+
+// const adharID = 3445678;
+// // adharID = 876544667;
+// console.log(adharID);
+
+// Local Scope
+ 
+// {
+//     const adharID = 876544667;
+//     console.log(adharID);
+// }
---
+++ LEC2/index.js
@@ -0,0 +1,84 @@
+//DTAT TYPE
+
+const { linearGradient } = require("framer-motion/client");
+
+// let num = 20;
+// console.log(typeof (num));
+
+// let str = 'Dipankar'
+// console.log(typeof (str));
+
+// let boolean = true
+// console.log(typeof boolean);
+
+// let a = null;
+// console.log(typeof (a));
+
+// let b ;
+// console.log(typeof b);
+
+// let a = Symbol("Code");
+// console.log(a);
+
+// let big = BigInt(123)
+// console.log(big);
+
+
+// OBJECT
+
+// const student = {
+//     Name : "Dipankar",
+//     RollNo : 21,
+// }
+// console.log(student);
+
+
+// Arithmatic operator
+
+// let sum = 4+7;
+// console.log(sum);
+
+// let multi = 3*8;
+// console.log(multi);
+
+// let div = 3/2;
+// console.log(div);
+
+
+// let div = 3%2;
+// console.log(div);
+
+// let pow = 2**5;
+// console.log(pow);
+
+// let name = 'Dipankar'
+
+// let val = 45;
+// // val += 10;
+// val -= 10;
+// console.log(val);
+
+// let count = 4;
+// // count++;
+// ++count;
+// console.log(count);
+
+// let count = 4;
+// // count--;
+// // --count;
+// console.log(count);
+
+// let count = 5;
+// // console.log(count++);
+// // console.log(++count);
+// // console.log(count--);
+// console.log(--count);
+
+
+// console.log(2 && 0);
+// console.log(0 && 2);
+// console.log(2 || 0);
+// console.log(0 || 2);
+
+let b;
+console.log(typeof (typeof (b)));
