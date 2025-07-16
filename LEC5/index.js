540b68a2f48063936baf867e909dee20f6097372
dipankar7645 <dipankar11092003@gmail.com>
2025-07-17 00:57:29 +0530

    commit

---
+++ LEC5/index.js
@@ -0,0 +1,13 @@
+// let arr = ['A', 'B', 'c', 'D']
+// // console.log(arr[2]);
+// // console.log(typeof (arr));
+// console.log(arr);
+
+// let language = ["Cpp","Java", "Python", "JavaScript", "C"]
+// console.log(language [3]);
+
+// let random = [1, "Hello", "sir", true, {name : "Dipankar"}, 2]
+// // console.log(random)
+// console.log( (typeof random[3]))
+
+
